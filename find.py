b47f73547c3838fefd66ae2a0e2db763f2356810
Cofee Coder <41608260+CoffeeCoder1015@users.noreply.github.com>
2020-11-23 23:44:24 +1300

    current weather function scrape update

--- find.py
+++ find.py
@@ -1,5 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
+import re
+
 header = {
     'User-Agent':'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
     'Accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
@@ -9,4 +11,8 @@ header = {
 }
 
 def current():
-    requests.get("https://weather.com/weather/todays").content
+    Req = requests.get("https://weather.com/en-NZ/weather/today/",headers=header)
+    print(Req.url)
+    Page = Req.text
+    Page_FMT = BeautifulSoup(Page,features='html.parser').prettify()
+    Page_FMT = "".join([len(re.findall(r"^(\s)",i))*3*" "+i+"\n" for i in Page_FMT.split("\n")])

f5ed7fd49f470d5087317099ef07967a9e953ce7
Cofee Coder <41608260+CoffeeCoder1015@users.noreply.github.com>
2020-11-23 20:58:31 +1300

    bot setup

--- WeatherMan.py
+++ WeatherMan.py
@@ -2,3 +2,14 @@ import f_opt
 
 from discord.ext import commands
 import discord
+
+client = commands.Bot(command_prefix="WMAN")
+
+def run(ID,mode):
+    client.run(ID,bot=mode)
+
+#option set to file name - 
+#so below only works when the script is ran with the exact file name - no "\", ".\", etc
+f_opt.f_opt({
+    "WeatherMan.py":([str,bool],run)
+})

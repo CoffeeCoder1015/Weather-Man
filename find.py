23f66746f52918d50331a7a34647c5f537b9a008
Cofee Coder <41608260+CoffeeCoder1015@users.noreply.github.com>
2020-12-04 15:19:59 +1300

    full implementation of searching

--- find.py
+++ find.py
@@ -27,6 +27,22 @@ def currentWeather():
     cur2 = weather_dat.find("div",{"class":"CurrentConditions--secondary--2XNLR"})
     return [tls,cur1.text,cur2.text]
 
+def search(location):
+    loc_ll = gsll(location)
+    loc_ll = f"{loc_ll[0]},{loc_ll[1]}"
+    Req = requests.get("https://weather.com/en-NZ/weather/today/l/"+loc_ll,headers=header)
+    print(Req.url)
+    Page = Req.text
+    Page_FMT = BeautifulSoup(Page,features='html.parser')
+    weather_dat = Page_FMT.find("div",{"id":"WxuCurrentConditions-main-b3094163-ef75-4558-8d9a-e35e6b9b1034"})
+    tlsblk = weather_dat.find("div",{"class":"CurrentConditions--header--3-4zi"})
+    location = tlsblk.find("h1",{"class":"CurrentConditions--location--1Ayv3"}).text
+    time = tlsblk.find("div",{"class":"CurrentConditions--timestamp--1SWy5"}).text
+    tls = f"{location} {time}"
+    cur1 = weather_dat.find("div",{"class":"CurrentConditions--primary--3xWnK"})
+    cur2 = weather_dat.find("div",{"class":"CurrentConditions--secondary--2XNLR"})
+    return [tls,cur1.text,cur2.text]
+
 def gsll(location):
     location = geolocator.geocode(str(location))
-    print(location.latitude,location.longitude)
+    return (location.latitude,location.longitude)
